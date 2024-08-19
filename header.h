e43f2a32e1b88006433acbca3b4bac12331d809c
Lka <lkamod433@gmail.com>
2024-08-20 06:22:21 +0700

    add header file

---
+++ header.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> Split(const string &, char);
+bool isInteger(const string &);
+vector<int> convertArrayToInt(vector<string> &array);
